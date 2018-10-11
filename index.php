1ca26dfc353364819acf094ead23e38c3c463b7c
Kerollin Andressa <kerollinandress@gmail.com>
2018-10-11 21:34:33 -0200

    TERCEIRO COMMIT DO PROJETO

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
+<?php require "conexao.php"?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset = "UTF-8"/>
-
 	<title>Mapear Barbearias</title>
 </head>	
 <body>
-	<div></div>
 	<a href="novo_salao.php">Novo Salão</a>
-<?php
-
-
-
-echo "";
-
-?>
-
 </body>
 </html>
