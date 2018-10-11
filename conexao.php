cbd5c83073471d881397b9431590ec6b7e0ce39a
Kerollin Andressa <kerollinandress@gmail.com>
2018-10-11 20:47:16 -0200

    SEGUNDO COMMIT DO PROJETO

--- conexao.php
+++ conexao.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset = "UTF-8"/>
-
-	<title>Mapear Barbearias</title>
-</head>	
-<body>
-	<div></div>
-	<a href="novo_salao.php">Novo Salão</a>
 <?php
 $servername = "localhost";
 $database = "mapear_barbearia";
 $username = "root";
 $password = "root";
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-
-
-echo "";
-
 ?>
-
-</body>
-</html>
 
--- novo_salao.php
+++ novo_salao.php
@@ -1,3 +1,4 @@
+<?php require "conexao.php"?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,20 +6,6 @@
 	<title>Mapear Barbearias</title>
 </head>	
 <body>
-<?php
-$servername = "localhost";
-$database = "mapear_barbearia";
-$username = "root";
-$password = "root";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Conectado com sucesso";
-mysqli_close($conn);
-?>
 
 </body>
 </html>
